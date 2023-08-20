bd097863086ac1b17001d57aef4da3ba2c4ff024
Maxim Tsoy <maxim.tsoy@team.bumble.com>
2023-08-20 18:36:28 +0400

    Upgrade to java 17 and kotlin 1.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.21"
+    kotlin("jvm") version "1.9.0"
 }
 
 version = "0.1.0"
@@ -19,6 +19,6 @@ subprojects {
     }
 
     tasks.withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.jvmTarget = "17"
     }
 }
