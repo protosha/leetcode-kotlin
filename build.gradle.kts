cebbda796271f9ad3df8a9eb3a5d56591830e781
Maxim Tsoy <maxim.tsoy@team.bumble.com>
2025-01-06 14:19:52 +0300

    Update gradle, update JVM, update kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-    kotlin("jvm") version "1.9.0"
+    kotlin("jvm") version "2.0.21"
 }
 
 version = "0.1.0"
@@ -12,11 +14,9 @@ subprojects {
         mavenCentral()
     }
 
-    dependencies {
-        implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.4")
-    }
-
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_21)
+        }
     }
 }
