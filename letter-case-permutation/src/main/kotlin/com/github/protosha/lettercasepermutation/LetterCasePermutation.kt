bc04b28a249750244f0c017ac3c5264f12200b2f
Maksim Tsoi <tsoimu@yandex.ru>
2025-07-25 11:22:18 +0100

    Small optimizations

--- letter-case-permutation/src/main/kotlin/com/github/protosha/lettercasepermutation/LetterCasePermutation.kt
+++ letter-case-permutation/src/main/kotlin/com/github/protosha/lettercasepermutation/LetterCasePermutation.kt
@@ -1,5 +1,8 @@
 package com.github.protosha.lettercasepermutation
 
+import java.util.LinkedList
+import java.util.Stack
+import javax.xml.stream.events.Characters
 import kotlin.math.pow
 
 object LetterCasePermutation {
@@ -44,30 +47,30 @@ class StackSolution {
         val totalLetters = s.filter { it.isLetter() }.length
         val totalElse = s.filter { !it.isLetter() }.length
         val totalPerms = ((2.0).pow(totalLetters) + totalElse).toInt()
-        val stack = ArrayList<StackOperation>(totalPerms)
-        stack.add(StackOperation(s, 0))
+        val stack = LinkedList<StackOperation>()
+        stack.add(StackOperation(s.toCharArray(), 0))
         val perms = ArrayList<String>(totalPerms)
         while (stack.isNotEmpty()) {
             val operation = stack.removeLast()
             if (operation.charAt <= operation.value.lastIndex) {
                 val value = operation.value
                 if (value[operation.charAt].isLetter()) {
-                    val left = value.substring(0, operation.charAt)
-                    val right = value.substring(operation.charAt + 1)
-                    val value1 = left + value[operation.charAt].uppercase() + right
-                    val value2 = left + value[operation.charAt].lowercase() + right
+                    val value1 = value.copyOf()
+                    value1[operation.charAt] = Character.toUpperCase(value[operation.charAt])
+                    val value2 = value.copyOf()
+                    value2[operation.charAt] = Character.toLowerCase(value[operation.charAt])
                     if (operation.charAt < operation.value.lastIndex) {
                         stack.add(StackOperation(value1, operation.charAt + 1))
                         stack.add(StackOperation(value2, operation.charAt + 1))
                     } else {
-                        perms.add(value1)
-                        perms.add(value2)
+                        perms.add(String(value1))
+                        perms.add(String(value2))
                     }
                 } else {
                     if (operation.charAt < operation.value.lastIndex) {
                         stack.add(StackOperation(value, operation.charAt + 1))
                     } else {
-                        perms.add(value)
+                        perms.add(String(value))
                     }
                 }
             }
@@ -75,8 +78,8 @@ class StackSolution {
         return perms
     }
 
-    data class StackOperation(
-        val value: String,
+    class StackOperation(
+        val value: CharArray,
         val charAt: Int
     )
 }
